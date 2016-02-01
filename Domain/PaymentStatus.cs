d3cd7060d74e35acbea255421d2429d34ff7909f
Maxim Gomon <flymandude@gmail.com>
2016-02-02 00:43:25 +0200

    Add new domain entities

---
+++ Domain/PaymentStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Состояние оплаты
+    /// </summary>
+    public class PaymentStatus : DictionaryEntity
+    {
+    }
+}
---
+++ Domain/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Тип оплаты (Наличными, на счет и т.д.)
+    /// </summary>
+    public class PaymentType : DictionaryEntity
+    {
+    }
+}
