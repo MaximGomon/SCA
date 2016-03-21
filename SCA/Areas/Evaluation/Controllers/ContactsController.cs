4d14ec4091d5ba6010a9175a496fafd3879ad082
Кравчук Максим <mak7.kr@gmail.com>
2016-03-21 11:24:10 +0200

    Changed List_Read

--- SCA/Areas/Evaluation/Controllers/ContactsController.cs
+++ SCA/Areas/Evaluation/Controllers/ContactsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 using SCA.Areas.Evaluation.Models;
 using SCA.BussinesLogic;
 using SCA.DataAccess.Repositories.Implementations;
@@ -16,7 +18,13 @@ namespace SCA.Areas.Evaluation.Controllers
     {
         private readonly ContactBusinessLogic _contactBusinessLogic = new ContactBusinessLogic(new ContactRepository());
         // GET: Monitoring/Contacts
-        public List<ContactModel> List()
+
+        public ActionResult List()
+        {
+            return View();
+        }
+        
+        public ActionResult List_Read([DataSourceRequest]DataSourceRequest request)
         {
             var items = _contactBusinessLogic.GetAllEntities().Select(x => new ContactModel
             {
@@ -27,7 +35,8 @@ namespace SCA.Areas.Evaluation.Controllers
                 Email = x.Email,
                 Id = x.Id
             });
-            return items.ToList();
+            DataSourceResult result = items.ToDataSourceResult(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
