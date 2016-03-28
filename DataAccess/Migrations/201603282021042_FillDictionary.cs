47d84477dbf40fa6046cab558631787d8139c619
Maxim Gomon <flymandude@gmail.com>
2016-03-28 23:30:10 +0300

    Add migration with dictionary fill script

---
+++ DataAccess/Migrations/201603282021042_FillDictionary.cs
@@ -0,0 +1,17 @@
+namespace SCA.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FillDictionary : DbMigration
+    {
+        public override void Up()
+        {
+            SqlFile("FillDictionary.sql");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
