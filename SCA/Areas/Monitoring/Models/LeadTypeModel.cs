d1f2f39c1d44ced473fc4c24badab2aad4bf68f9
Maxim Gomon <flymandude@gmail.com>
2016-04-22 23:34:23 +0300

    Add lead type model

---
+++ SCA/Areas/Monitoring/Models/LeadTypeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SCA.Domain;
+
+namespace SCA.Areas.Monitoring.Models
+{
+    public class LeadTypeModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Tags { get; set; } 
+    }
+}
